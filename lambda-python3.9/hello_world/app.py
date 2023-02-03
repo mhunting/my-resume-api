790e9a43a138bd695a98e496418bc66e0b9afe92
mikee <megnmike@gmail.com>
2023-02-03 04:18:27 +0000

    adding dynamodb updates

--- lambda-python3.9/hello_world/app.py
+++ lambda-python3.9/hello_world/app.py
@@ -1,42 +1,42 @@
 import json
+import boto3
+import botocore
 
-# import requests
-
+from datetime import datetime, timezone
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-    print(json.dumps(event))
-    return {
+   
+    client = boto3.client('dynamodb')
+    
+    ip = event['requestContext']['identity']['sourceIp']
+    client.put_item(TableName='mh-resume', Item={'pk':{'S':'visitor'},'sk':{'S': ip}, 'date':{'S': datetime.now(timezone.utc).strftime('%Y%m%d')}})
+    try:
+        response = client.update_item(TableName='mh-resume',
+            Key={'pk': {'S':'visitor-stats'}, 'sk':{'S':'total-count'}},
+            UpdateExpression="set stat_value = stat_value + :i",
+            ExpressionAttributeValues={':i': {'N':'1'}},         
+            ReturnValues='UPDATED_NEW')        
+        return {
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "https://www.mh-resume.net",
+            "Access-Control-Allow-Methods": "OPTIONS,GET"
+        },
         "body": json.dumps({
+            "visitorCount": response['Attributes']['stat_value']["N"],
             "message": "hello world",
             # "location": ip.text.replace("\n", "")
         }),
-    }
+        }
+    except botocore.exceptions.ClientError as err:
+        print(err)      
+        return {
+            'statusCode': 200,
+             "headers": {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "https://www.mh-resume.net",
+            "Access-Control-Allow-Methods": "OPTIONS,GET"
+        },
+            'visitorCount':0
+        }
